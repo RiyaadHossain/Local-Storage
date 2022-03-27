72f9ed3a8251e8fcb4d523f217c679c82cb81022
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-03-27 15:14:36 +0600

    Function: To Check existense Item in the Database

--- JS/app.js
+++ JS/app.js
@@ -1,32 +1,37 @@
-
 /* ========================= Function: To Add Item in the Database ========================= */
 
-const addToDB = item => {
-    let cart = {}
-    //  Get the Stored Item 
-    const storedItem = localStorage.getItem('storedItem')
-    if (storedItem) {
-        cart = JSON.parse(storedItem) 
-    }
+const addToDB = (item) => {
+  let cart = {};
+  //  Get the Stored Item
+  const storedItem = localStorage.getItem("storedItem");
+  if (storedItem) {
+    cart = JSON.parse(storedItem);
+  }
 
-    // Set the actual Value 
-    const quantity = cart[item]
-    quantity ? cart[item] = quantity + 1 : cart[item] = 1
-      
-    // Stored the Updated value
-    localStorage.setItem('storedItem', JSON.stringify(cart))
-}
+  // Set the actual Value
+  const quantity = cart[item];
+  quantity ? (cart[item] = quantity + 1) : (cart[item] = 1);
 
+  // Stored the Updated value
+  localStorage.setItem("storedItem", JSON.stringify(cart));
+};
+
+/* ========================= Function: To Check existense Item in the Database ========================= */
+
+const itemChecker = (item) => {
+    const cart = JSON.parse(localStorage.getItem('storedItem'))
+    item in cart ? console.log("Yesss, It is exixt in the Cart.") : console.log("Noo, It isn't in the cart.")
+};
 
 /* ========================= Function: To Delete Item in the Database ========================= */
 
-const deleteItem = item => {
-    // Geth the Stored Item 
-    const storedItem = JSON.parse(localStorage.getItem('storedItem'))
-    if (storedItem) {
-        delete storedItem[item]
-    }
+const deleteItem = (item) => {
+  // Geth the Stored Item
+  const storedItem = JSON.parse(localStorage.getItem("storedItem"));
+  if (storedItem) {
+    delete storedItem[item];
+  }
 
-    // Set the Updated Value 
-    localStorage.setItem('storedItem', JSON.stringify(storedItem))
-}
+  // Set the Updated Value
+  localStorage.setItem("storedItem", JSON.stringify(storedItem));
+};
